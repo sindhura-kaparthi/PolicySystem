b90ddcdb7ad79bd37c90b0e380e60a6740a8a052
sindhura-kaparthi <sindhura.kaparthi@thoughtworks.com>
2023-06-06 14:57:42 +0530

    Revert "Add. Validation for claim number using validation interceptor"
    This reverts commit b36810697e1a825381983a10ec796e8993cdaa7b.

--- src/main/java/com/example/policysystem/controller/PolicyDetailsEndpoint.java
+++ src/main/java/com/example/policysystem/controller/PolicyDetailsEndpoint.java
@@ -1,5 +1,6 @@
 package com.example.policysystem.controller;
 
+import com.example.policysystem.exception.InvalidClaimNumberException;
 import com.example.policysystem.policies.GetPolicyDetailsRequest;
 import com.example.policysystem.policies.GetPolicyDetailsResponse;
 import com.example.policysystem.policies.PolicyDetails;
@@ -12,20 +13,23 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class PolicyDetailsEndpoint {
-    final PolicyDetailsService policyDetailsService;
-
     @Autowired
-    public PolicyDetailsEndpoint(PolicyDetailsService policyDetailsService) {
-        this.policyDetailsService = policyDetailsService;
-    }
+    PolicyDetailsService policyDetailsService;
+    private static final String INVALID_CLAIM_NUMBER = "Claim Number is not valid";
 
     @PayloadRoot(namespace = "http://policysystem.com/policies", localPart = "GetPolicyDetailsRequest")
     @ResponsePayload
-    public GetPolicyDetailsResponse getPolicyDetails(@RequestPayload GetPolicyDetailsRequest request) {
-        PolicyDetails policyDetails = policyDetailsService.getPolicyDetails();
-        GetPolicyDetailsResponse response = new GetPolicyDetailsResponse();
-        response.setPolicyDetails(policyDetails);
-        return response;
+    public GetPolicyDetailsResponse getPolicyDetails(@RequestPayload GetPolicyDetailsRequest request) throws InvalidClaimNumberException {
+        if (validateClaimNumber(request.getClaimNumber())){
+            PolicyDetails policyDetails = policyDetailsService.getPolicyDetails();
+            GetPolicyDetailsResponse response = new GetPolicyDetailsResponse();
+            response.setPolicyDetails(policyDetails);
+            return response;
+        }
+        else throw new InvalidClaimNumberException(INVALID_CLAIM_NUMBER);
     }
 
+    private boolean validateClaimNumber(String claimNumber) {
+        return claimNumber.matches("^[A-Za-z]{3}-\\d{6}$");
+    }
 }
